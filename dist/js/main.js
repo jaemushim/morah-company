77794bc5577fcfcff9cb6d6764b245eb5a3b119a
심재무 <shimwoan@gmail.com>
2021-03-12 06:52:33 +0900

    first

---
+++ dist/js/main.js
@@ -0,0 +1,107 @@
+$(function () {
+    // 접근성 본문 바로가기
+    $('.accessibility a').on('focus', function () {
+        $(this).css('top', 0);
+    });
+    $('.accessibility a').focusout(function () {
+        $(this).css('top', '-50px');
+    });
+
+    // 메인 비쥬얼 슬라이더
+    $('.main-visual-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        speed: 1000,
+    });
+
+    // GNB 드롭다운메뉴
+    $('.gnb .dept-1>li>a, .gnb .dept-2').hover(
+        function () {
+            $(this).parent().find('.dept-2').css('display', 'block');
+        },
+        function () {
+            $(this).parent().find('.dept-2').css('display', 'none');
+        }
+    );
+
+    // 서브페이지 현재 메뉴
+    $('.subpage-curr .dept-1 a')
+        .not($('.dept-2 > li > a,.home'))
+        .on('click', function (e) {
+            e.preventDefault();
+            $('.subpage-curr .dept-1>li').not($(this).parent()).removeClass('active');
+            $(this).parent().toggleClass('active');
+        });
+
+    // 회원가입 Step-2 메뉴 select 클릭시 화살표 active
+    $('.sign_step-2 select').click(function () {
+        $(this).toggleClass('active');
+    });
+
+    // 회원가입 체크박스 전체 선택
+    $('#agree-all').on('click', function () {
+        $(this).parents('.content').find('.agree').prop('checked', true);
+    });
+
+    // 제품소개 해골 호버시 밑줄 빨간색 효과
+    $('.product-intro .row .col-1 a').hover(function () {
+        $(this).parent().toggleClass('on');
+    });
+});
+$('.product-nav').slick({
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 3,
+    arrows: true,
+    speed: 300,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 3,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+            },
+        },
+    ],
+});
+
+$('.product-list-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    arrows: true,
+    speed: 300,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
